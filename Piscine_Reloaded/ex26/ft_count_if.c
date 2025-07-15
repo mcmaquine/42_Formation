f8e744c5e9908dabdfb7afa31f616dccc515e2fe
Matheus Maquine <mmaquine@c2r6p6.42sp.org.br>
2025-07-15 14:20:10 -0300

    modificacao no ex26

--- Piscine_Reloaded/ex26/ft_count_if.c
+++ Piscine_Reloaded/ex26/ft_count_if.c
@@ -6,7 +6,7 @@
 /*   By: mmaquine <mmaquine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 20:54:56 by mmaquine          #+#    #+#             */
-/*   Updated: 2025/07/15 11:59:11 by mmaquine         ###   ########.fr       */
+/*   Updated: 2025/07/15 14:18:11 by mmaquine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ int	ft_count_if(char **tab, int (*f)(char*))
 
 	i = 0;
 	count = 0;
-	while (tab[i][0] != '\0')
+	while (tab[i] != 0)
 	{
-		if ((*f)(tab[i]))
+		if (f(tab[i]) == 1)
 			count++;
 		i++;
 	}
